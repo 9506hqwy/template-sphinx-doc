3e5eecba5d190d0861a43751f4eac01e88893dbc
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-11-12 11:25:56 +0900

    tex の設定を別のファイルに移行した。

--- conf.py
+++ conf.py
@@ -20,6 +20,7 @@ extensions = [
 
 exclude_patterns = [
     '_build',
+    '_latex',
     '_script',
 ]
 
@@ -39,89 +40,12 @@ html_style = 'css/styles.css'
 # -- Options for PDF output --------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
 
-latex_passoptionstopackages = rf'''
-\makeatletter
-\PassOptionsToPackage{{
-    pdftitle={{{project}}},
-    pdfauthor={{{author}}}
-}}{{hyperref}}
-\makeatother
-'''
-
-latex_preamble = r'''
-% 透かしの設定
-\DraftwatermarkOptions{angle={0}}
-\DraftwatermarkOptions{scale={0.15}}
-\DraftwatermarkOptions{hpos={0.90\paperwidth}}
-\DraftwatermarkOptions{vpos={0.20\paperwidth}}
-\DraftwatermarkOptions{color={[rgb]{1, 0, 0}}}
-\DraftwatermarkOptions{text={%
-    \includegraphics{./../../images/maruhi_mark.png} \\ サンプル
-}}
-
-% ヘッダとフッタを設定
-% FIXED ME: chapter の最初のページが変更できない。
-\renewcommand{\sectionmark}[1]{\markright{\thesection.\ #1}}
-
-\fancypagestyle{normal}{
-    \fancyhf{}
-    \fancyhead[LE]{\rightmark}
-    \fancyhead[RO]{\rightmark}
-    \fancyfoot[C]{\thepage}
-    \renewcommand{\headrulewidth}{0.4pt}
-    \renewcommand{\footrulewidth}{0.4pt}
-}
-
-% 表目次と図目次を追加
-\makeatletter
-\renewcommand{\sphinxtableofcontents}{
-    \pagenumbering{roman}
-    \begingroup
-        \parskip
-        \z@skip
-        \sphinxtableofcontentshook
-        \tableofcontents
-        \listoffigures
-        \listoftables
-    \endgroup
-    \if@openright
-        \cleardoublepage
-    \else
-        \clearpage
-    \fi
-    \pagenumbering{arabic}
-}
-\makeatother
-
-% 目次にスタイルを適用
-\AtBeginDocument{\addtocontents{toc}{\protect\thispagestyle{normal}}}
-\AtBeginDocument{\addtocontents{lof}{\protect\thispagestyle{normal}}}
-\AtBeginDocument{\addtocontents{lot}{\protect\thispagestyle{normal}}}
-'''
-
-latex_maketitle = r'''
-\makeatletter
-\begin{titlepage}
-    \ThisULCornerWallPaper{0.25}{./../../images/mark_network_status_benkyouchu.png}
-    \vspace*{\fill}
-    \begin{center}
-        \py@HeaderFamily
-        {\Huge \@title \par}
-        {\itshape\LARGE \py@release\releaseinfo \par}
-    \end{center}
-    \vspace*{\fill}
-    \begin{flushright}
-        {\LARGE \@date \par \@author}
-    \end{flushright}
-    \vspace*{\baselineskip}
-\end{titlepage}
-\makeatother
-'''
-
-latex_extrapackages = r'''
-\usepackage{draftwatermark}
-\usepackage{wallpaper}
-'''
+latex_passoptionstopackages = open('_latex/pass_options_to_packages.tex') \
+    .read() \
+    .format(project=project, author=author)
+latex_preamble = open('_latex/preamble.tex').read()
+latex_maketitle = open('_latex/maketitle.tex').read()
+latex_extrapackages = open('_latex/extra_packages.tex').read()
 
 latex_elements = {
     'papersize': 'a4paper',
